6a65af89cface4b5ba552d3764e516dbb750946d
James Cliff <me@jamescliff.com>
2021-01-18 12:04:05 -0500

    :zap: update env keys for deployment

---
+++ secret/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+module.exports = config = {
+
+	// SPOTIFY
+	client_id: process.env.SECRET_SPOTIFY_CLIENT_ID,
+	client_secret: process.env.SECRET_SPOTIFY_CLIENT,
+	redirect_uri: 'http://localhost:8888/callback',
+
+	// FIREBASE
+	type: "service_account",
+	project_id: "shuffle-devtest",
+	private_key_id: process.env.SECRET_FIREBASE_KEY_ID,
+	private_key: process.env.SECRET_FIREBASE_KEY.replace(/\\n/g, '\n'),
+	client_email: "firebase-adminsdk-fppxu@shuffle-devtest.iam.gserviceaccount.com",
+	firebase_client_id: process.env.SECRET_FIREBASE_CLIENT_ID,
+	auth_uri: "https://accounts.google.com/o/oauth2/auth",
+	token_uri: "https://oauth2.googleapis.com/token",
+	auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+	client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fppxu%40shuffle-devtest.iam.gserviceaccount.com"
+	
+}
