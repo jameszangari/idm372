1cda6808a7e104f691cda986f24a5a47d162b3b1
James Cliff <me@jamescliff.com>
2021-01-18 14:24:41 -0500

    :zap: update env keys for deployment

--- src/scripts/firebase.js
+++ src/scripts/firebase.js
@@ -6,7 +6,9 @@ module.exports = {
     firebaseAdmin.initializeApp({ // Init Admin SDK w/ Keys
       // credential: firebaseAdmin.credential.cert(config)
       credential: firebaseAdmin.credential.cert({
+        project_id: "shuffle-devtest",
         private_key: process.env.SECRET_FIREBASE_KEY.replace(/\\n/g, '\n'),
+        client_email: "firebase-adminsdk-fppxu@shuffle-devtest.iam.gserviceaccount.com",
       }),
     });
   },
