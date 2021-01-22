7fd0b425a3093777bb6288c015bbc4882afbbf0e
Hunter Hampton <hmh84@drexel.edu>
2021-01-22 14:16:00 -0500

    :x: Disable Spaces Only Inputs
    * Added listener to every text input, on change, trim the value. That will prevent spaces and ultimately empty fields while combined with previous enhancments

--- src/scripts/modules/Validate.js
+++ src/scripts/modules/Validate.js
@@ -2,35 +2,50 @@ const helper = require('../helper');
 
 module.exports = {
     init: function () {
-        const date_inputs = docQA('input[type="date"]');
-        if (date_inputs.length > 0) {
-            // Set 18 y/o restriction
-            var today = new Date();
-            var dd = today.getDate();
-            var mm = today.getMonth() + 1; // January is 0!
-            var yyyy = today.getFullYear();
-            if (dd < 10) {
-                dd = '0' + dd
-            }
-            if (mm < 10) {
-                mm = '0' + mm
-            }
-            const max = (yyyy - 18) + '-' + mm + '-' + dd;
-            const min = (yyyy - 90) + '-' + mm + '-' + dd;
-
-            date_inputs.forEach(el => { // Set 'Max' attribute
-                el.setAttribute('max', max);
-                el.setAttribute('min', min);
-            });
-        }
+        // If there's a form
         const next = docQ('[data-form]');
         if (!next) { return }
         else { // If the next button has a form to submit before navigating
+            // Prevent spaces around text
+            const req_strings = docQA('input[type="text"]');
+            if (req_strings.length > 0) { // If there's string input(s)
+                req_strings.forEach(el => {
+                    if (el.required) {
+                        el.addEventListener('change', () => { // Trim on every change
+                            el.value = el.value.trim();
+                        });
+                    }
+                });
+            }
+            // Set min & max dates
+            const date_inputs = docQA('input[type="date"]');
+            if (date_inputs.length > 0) { // If there's date input(s)
+                // Set 18 y/o restriction
+                var today = new Date();
+                var dd = today.getDate();
+                var mm = today.getMonth() + 1; // January is 0!
+                var yyyy = today.getFullYear();
+                if (dd < 10) {
+                    dd = '0' + dd
+                }
+                if (mm < 10) {
+                    mm = '0' + mm
+                }
+                const max = (yyyy - 18) + '-' + mm + '-' + dd;
+                const min = (yyyy - 90) + '-' + mm + '-' + dd;
+
+                date_inputs.forEach(el => { // Set 'Max' attribute
+                    el.setAttribute('max', max);
+                    el.setAttribute('min', min);
+                });
+            }
+
             // quickRefs
             const url = next.href,
                 form = docQ(next.dataset.form),
                 type = form.getAttribute('type');
 
+            // If form type is a 'check just one per section'
             if (type === 'checkOne') {
                 // Handle checkbox clicks & requires
                 const sections = form.querySelectorAll('section');
@@ -50,8 +65,10 @@ module.exports = {
                 });
             }
 
+            // What happens when you click the next button
             next.addEventListener('click', (e) => {
                 e.preventDefault(); // Stop anchor from changing page location by default
+
                 if (form.checkValidity()) { // If form is valid
                     // Create submit button
                     const submit = document.createElement('button');
@@ -77,8 +94,8 @@ module.exports = {
                         invalids.forEach(el => {
                             el.classList.add('invalid'); // Located an invalid input
                             // Check date input
-                            el.addEventListener('change', () => {
-                                el.value.length > 0 ? el.classList.remove('invalid') : el.classList.add('invalid');
+                            el.addEventListener('input', () => {
+                                el.value.trim().length > 0 ? el.classList.remove('invalid') : el.classList.add('invalid');
                             });
                         });
                     }
