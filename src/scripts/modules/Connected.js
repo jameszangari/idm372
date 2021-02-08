90f44c609d9c99f665f5aaac23f481f35b5b9076
Hunter Hampton <hmh84@drexel.edu>
2021-02-08 04:08:46 -0500

    :sparkles: Add Top 3's to DB

--- src/scripts/modules/Connected.js
+++ src/scripts/modules/Connected.js
@@ -10,11 +10,7 @@ const spotifyObjectString = helper.getCookie('spotify');
 const spotifyObject = JSON.parse(spotifyObjectString);
 
 if (lconnected) { //if on right page
-	let itemName = docQA('.l-connected__artists--item-text');
-	let itemImage = docQA('.l-connected__artists--item-image');
-	console.log(itemName[1]);
-
-	function sendrequest(type, i) {
+	function sendrequest(type) {
 		$.ajax({ //send info to server - GET request
 			url: endpoints.connected.url,
 			data: {
@@ -24,16 +20,45 @@ if (lconnected) { //if on right page
 			}
 		}).done(function (results) { //receive info, populate html, add event listeners to tracks to add as anthem
 			// Add tracks to page in search results
-
-			results.forEach(result => {
-				if (result.name) { // Skip the empty results
-					itemName[i].innerHTML = result.name;
-					itemImage[i].src = result.thumb;
+			const form = docQ('#firestore_form');
+			
+			if (results.artists.length > 0) {
+				const artists = results.artists;
+				let i = 0;
+				artists.forEach(artist => {
+					form.innerHTML += `
+					<input type="text" name="artist-${i}-id" value="${artist.id}"></input>
+					<input type="text" name="artist-${i}-title" value="${artist.title}"></input>
+					<input type="text" name="artist-${i}-href" value="${artist.href}"></input>
+					<input type="text" name="artist-${i}-thumb" value="${artist.thumb}"></input>
+					`;
+					const section = docQ('section.group--artists');
+					const imgEl = section.querySelectorAll('.l-connected__item-image')[i];
+					imgEl.src = artist.thumb;
+					const nameEl =  section.querySelectorAll('.l-connected__item-text')[i];
+					nameEl.innerText = helper.truncateString(artist.title, 30);
 					i++;
-				}
-			});
+				});
+			} else if (results.tracks.length > 0) {
+				const tracks = results.tracks;
+				let i = 0;
+				tracks.forEach(track => {
+					form.innerHTML += `
+					<input type="text" name="track-${i}-id" value="${track.id}"></input>
+					<input type="text" name="track-${i}-title" value="${track.title}"></input>
+					<input type="text" name="track-${i}-href" value="${track.href}"></input>
+					<input type="text" name="track-${i}-thumb" value="${track.thumb}"></input>
+					`;
+					const section = docQ('section.group--tracks');
+					const imgEl = section.querySelectorAll('.l-connected__item-image')[i];
+					imgEl.src = track.thumb;
+					const nameEl =  section.querySelectorAll('.l-connected__item-text')[i];
+					nameEl.innerText = helper.truncateString(track.title, 30);
+					i++;
+				});
+			}
 		});
 	}
-	sendrequest('artists', 0);
-	sendrequest('tracks', 3);
+	sendrequest('artists');
+	sendrequest('tracks');
 }
